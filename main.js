872b1d3beb93b4dd88efd83a30e43e00f69def62
jeremy <spirography@users.noreply.github.com>
2018-02-17 12:33:12 -0500

    set up js skeleton

---
+++ main.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', init, false);
+/*
+ * Initializes everything
+ */
+function init() {
+	console.log('page loaded, initializing...');
+
+	// create the graph
+	createGraph();
+
+	// add the svg map to the page
+
+
+	// set up event handlers
+}
+
+
+/*
+ * Build the graph and save it in JSON form
+ */
+function createGraph() {
+
+}
+
+
+/*
+ * 
+ */
+function djikstra(s, t, options=[]) {
+
+}
