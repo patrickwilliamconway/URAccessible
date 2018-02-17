7abe8f0f097cd635d7d509d542e214cfce0efe1f
jeremy <spirography@users.noreply.github.com>
2018-02-17 16:29:58 -0500

    started work on actual map

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/*
+ * Constants and other global information
+ */
+var svgns = "http://www.w3.org/2000/svg";
+var svg;
+
+
 window.addEventListener('load', init, false);
 /*
  * Initializes everything
@@ -9,7 +16,8 @@ function init() {
 	createGraph();
 
 	// add the svg map to the page
-
+	createSVG();
+	document.body.appendChild(svg);
 
 	// set up event handlers
 }
@@ -20,6 +28,67 @@ function init() {
  */
 function createGraph() {
 
+}
+
+/*
+ * Build the SVG file used for holding the traversable edges
+ */
+function createSVG() {
+	svg = document.createElementNS(svgns, "svg");
+	svg.setAttribute("BaseProfile", "tiny");
+	svg.setAttribute("version", "1.2")
+	svg.setAttribute("width", "1150");
+	svg.setAttribute("height", "950");
+	svg.setAttribute("xmlns", svgns);
+
+
+
+	/*
+	 * BUILDING INFORMATION
+	 */
+	
+
+	/*
+	 * PATH INFORMATION
+	 */
+	var pathways = document.createElementNS(svgns, "g");
+	EDGE_DATA.forEach(function(e) {
+        var polyline = document.createElementNS(svgns, "polyline");
+        polyline.setAttribute("fill", "none");
+        polyline.setAttribute("stroke-width", "2");
+        polyline.setAttribute("stroke", "black");
+
+        var string = ""
+        for (var i = 0; i < e.length; i += 2) {
+            string += e[i] + "," + e[i+1] + " ";
+        }
+        polyline.setAttribute("points", string);
+
+        // event handling for hoverover while testing
+        polyline.addEventListener("mouseenter", function(e) {
+
+            this.setAttribute("stroke", "red");
+            this.setAttribute("stroke-width", 5);
+
+        }, false);
+        polyline.addEventListener("mouseleave", function(e) {
+            // USE TRANSPARENT, NOT NONE
+            this.setAttribute("stroke", "black");
+            this.setAttribute("stroke-width", 2);
+        }, false);
+
+        polyline.addEventListener("click", function(e) {
+
+            console.log(this);
+
+        }, false);
+
+
+        pathways.appendChild(polyline);
+    });
+    
+
+    svg.appendChild(pathways);
 }
 
 
