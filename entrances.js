e6ad9ddedbd93167b804795ab48d9787225d01a8
jeremy <spirography@users.noreply.github.com>
2018-02-17 22:48:41 -0500

    added entrances.js

---
+++ entrances.js
@@ -0,0 +1,380 @@
+var ENTRANCE_DATA = [
+	{
+	"building": "Gilbert",
+	"x": 248.9919,
+	"y": 201.1549,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Tiernan",
+	"x": 177.5014,
+	"y": 237.0471,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Hoeing",
+	"x": 276.3691,
+	"y": 260.5694,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Lovejoy",
+	"x": 205.8108,
+	"y": 295.6712,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Crosby",
+	"x": 292.6705,
+	"y": 308.6779,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Burton",
+	"x": 232.4913,
+	"y": 337.0802,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Goergen Athletic Center",
+	"x": 411.9975,
+	"y": 322.0692,
+	"accessible": true,
+	"desc": "Main entrance"
+	},
+	{
+	"building": "Goergen Athletic Center",
+	"x": 349.0683,
+	"y": 256.1531,
+	"accessible": true,
+	"desc": "West side of building - CONNECTED ON GRAPH"
+	},
+	{
+	"building": "Fauver Stadium",
+	"x": 441.7255,
+	"y": 237.0811,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "O'Brien",
+	"x": 726.0531,
+	"y": 91.5082,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Wilder",
+	"x": 739.4606,
+	"y": 135.136,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Sage",
+	"x": 691.8372,
+	"y": 164.5693,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Sue B.",
+	"x": 607.3321,
+	"y": 308.0531,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "University Health Service",
+	"x": 590.3039,
+	"y": 360.5011,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Douglass Commons",
+	"x": 428.2677,
+	"y": 356.4229,
+	"accessible": true,
+	"desc": "North Entrance - across from Athletic Center"
+	},
+	{
+	"building": "Douglass Commons",
+	"x": 422.4381,
+	"y": 390.5954,
+	"accessible": true,
+	"desc": "North East Entrance - Patio 1 - into building"
+	},
+	{
+	"building": "Douglass Commons",
+	"x": 432.3871,
+	"y": 399.7784,
+	"accessible": true,
+	"desc": "North East Entrance - Patio 2 - into Grab and Go"
+	},
+	{
+	"building": "Douglass Commons",
+	"x": 447.1759,
+	"y": 407.8461,
+	"accessible": true,
+	"desc": "South Entrance"
+	},
+	{
+	"building": "Wilson Common",
+	"x": 370.6722,
+	"y": 359.7116,
+	"accessible": true,
+	"desc": "north entrance - from res quad"
+	},
+	{
+	"building": "Wilson Commons",
+	"x": 392.9964,
+	"y": 390.7892,
+	"accessible": true,
+	"desc": "Under bridge from Douglass"
+	},
+	{
+	"building": "Wilson Commons",
+	"x": 391.5548,
+	"y": 405.1869,
+	"accessible": true,
+	"desc": "Bridge from academic quad"
+	},
+	{
+	"building": "Morey",
+	"x": 386.152,
+	"y": 444.2367,
+	"accessible": true,
+	"desc": "Quad-side entrance"
+	},
+	{
+	"building": "Morey",
+	"x": 381.2624,
+	"y": 429.9851,
+	"accessible": true,
+	"desc": "back side entrance"
+	},
+	{
+	"building": "Rettner",
+	"x": 350.0946,
+	"y": 442.7598,
+	"accessible": true,
+	"desc": "backside of Lattimore"
+	},
+	{
+	"building": "Rettner",
+	"x": 332.3147,
+	"y": 451.2679,
+	"accessible": true,
+	"desc": "from LeChase courtyard"
+	},
+	{
+	"building": "Rettner",
+	"x": 325.3401,
+	"y": 416.2647,
+	"accessible": true,
+	"desc": "north entrance by Wilson Commons"
+	},
+	{
+	"building": "Lattimore",
+	"x": 294.2517,
+	"y": 489.4249,
+	"accessible": true,
+	"desc": "main entrance from academic quad"
+	},
+	{
+	"building": "LeChase",
+	"x": 267.9795,
+	"y": 408.1545,
+	"accessible": true,
+	"desc": "main enterance 1"
+	},
+	{
+	"building": "LeChase",
+	"x": 226.426,
+	"y": 425.2058,
+	"accessible": true,
+	"desc": "main enterance 2"
+	},
+	{
+	"building": "Strong Auditorium",
+	"x": 215.0065,
+	"y": 532.5539,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Interfaith Chapel",
+	"x": 159.825,
+	"y": 587.7529,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Schlegel",
+	"x": 248.5263,
+	"y": 566.8108,
+	"accessible": true,
+	"desc": "Across from Strong Auditorium"
+	},
+	{
+	"building": "Schlegel",
+	"x": 276.3448,
+	"y": 587.6774,
+	"accessible": true,
+	"desc": "across from Dewey"
+	},
+	{
+	"building": "Dewey",
+	"x": 319.2711,
+	"y": 544.3178,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Bausch & Lomb",
+	"x": 398.2314,
+	"y": 509.0406,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Hoyt",
+	"x": 369.764,
+	"y": 549.7051,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Meliora",
+	"x": 500,
+	"y": 510,
+	"accessible": true,
+	"desc": "not exact point - this is a guess"
+	},
+	{
+	"building": "Harkness",
+	"x": 565.7262,
+	"y": 573.5431,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Gavett",
+	"x": 378.665,
+	"y": 637.9279,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Hopeman",
+	"x": 355.2389,
+	"y": 641.4786,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Wallis",
+	"x": 277.4821,
+	"y": 659.4618,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Wegmans",
+	"x": 350.71,
+	"y": 707.4663,
+	"accessible": true,
+	"desc": "Front of building"
+	},
+	{
+	"building": "Wegmans",
+	"x": 309.975,
+	"y": 708.6284,
+	"accessible": true,
+	"desc": "Hutchinson road enterance"
+	},
+	{
+	"building": "Taylor",
+	"x": 392.2909,
+	"y": 683.9206,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Goergen",
+	"x": 380.1275,
+	"y": 737.4341,
+	"accessible": true,
+	"desc": "Statue enterance"
+	},
+	{
+	"building": "Goergen",
+	"x": 371.8515,
+	"y": 778.2569,
+	"accessible": true,
+	"desc": "main entrance"
+	},
+	{
+	"building": "Goergen",
+	"x": 398.9104,
+	"y": 788.9652,
+	"accessible": true,
+	"desc": "intercampus drive - back entrance"
+	},
+	{
+	"building": "CSB",
+	"x": 324.1144,
+	"y": 810.2752,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Hutchinson",
+	"x": 296.4575,
+	"y": 786.326,
+	"accessible": true,
+	"desc": ""
+	},
+	{
+	"building": "Hylan",
+	"x": 308.1389,
+	"y": 770.7871,
+	"accessible": true,
+	"desc": "top enterance by Hutchinson"
+	},
+	{
+	"building": "Hylan",
+	"x": 297.8361,
+	"y": 765.6834,
+	"accessible": true,
+	"desc": "basement entrance"
+	},
+	{
+	"building": "Rush Rhees",
+	"x": 511.5394,
+	"y": 416.165,
+	"accessible": true,
+	"desc": "tunnel enterance by library lot"
+	},
+	{
+	"building": "Lattimore",
+	"x": 258.723,
+	"y": 485.9862,
+	"accessible":true,
+	"desc": "tunnel enterance by Todd Union"
+	},
+	{
+	"building": "Dewey",
+	"x": 315.5448,
+	"y": 577.345,
+	"accessible": true,
+	"desc": "tunnel enterance across from Simon"
+	},
+];
